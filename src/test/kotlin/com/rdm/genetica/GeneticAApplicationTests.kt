ddd338bae5a883863fe8e111828d2c3697602085
RDMotta <muitopoco@gmail.com>
2023-07-31 15:58:34 -0300

    Create project GA

---
+++ src/test/kotlin/com/rdm/genetica/GeneticAApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rdm.genetica
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeneticAApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
